685c240ca634356403919ed2b3d2ea6f6b663d8d
Érika Yukari Ferreira Kamikava <89429950+erikayukari@users.noreply.github.com>
2021-12-19 20:43:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def dados_youtube():
-    chave= os.environ["YOUTUBE_API_KEY"]
-    canal="UCqRBKOf_OvnNNvmbhVVbX-A"
-    url_canal = f"https://www.googleapis.com/youtube/v3/channels?part=id%2Csnippet%2CcontentDetails%2Cstatistics&id={canal}&key={chave}"
-    resposta = requests.get(url_canal)
-    dados_canal = resposta.json()
-    return dados_canal
-
+#início raspagem -----------------------------------
+!pip install GoogleNews #instala a biblioteca      
+from GoogleNews import GoogleNews 
+googlenews = GoogleNews()
+googlenews.set_time_range(start='31/12/2019', end='17/12/2021') #seleciona a janela temporal da busca
+#fim raspagem---------------------------------------
 @app.route("/")
 def hello_world():
     return render_template("home.html")
@@ -27,13 +25,15 @@ def sobre():
 def youtube():
    dados_canal = dados_youtube()
    return render_template("youtube.html", dados=dados_canal)
+  
+ #início telegram ------------------------------------
 
 from flask import request
 import requests
 
 @app.route("/telegram", methods=["POST"])
 def telegram():
-    
+        
     #Robô processa mensagem
     token = os.environ["TELEGRAM_TOKEN"]
     update = request.json
