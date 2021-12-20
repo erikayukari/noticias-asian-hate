9f824903339a314e5c1f73f86ce76f711e781a2a
Érika Yukari Ferreira Kamikava <89429950+erikayukari@users.noreply.github.com>
2021-12-20 02:48:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from GoogleNews import GoogleNews
 app = Flask(__name__)
 
 
-
-
 @app.route("/")
 def hello_world():
     return render_template("home.html")
@@ -20,17 +18,17 @@ def sobre():
 @app.route('/noticias')
 def news():
   googlenews = GoogleNews()
-  googlenews.set_time_range(start='31/12/2019', end='31/12/2019') #seleciona a janela temporal da busca
+  googlenews.set_time_range(start='31/12/2019', end='31/12/2021') #seleciona a janela temporal da busca
   googlenews.get_news("'preconceito amarelo'") #seta o primeiro termo de busca 
   googlenews.get_news("'preconceito asiático'") 
   googlenews.get_news("'vírus chinês'")
   googlenews.get_news("'violência contra asiáticos'")
-  googlenews.get_news("'ódio contra asiáticos'")
+  googlenews.get_news("'ódio contra asiáticos'") #seta o último termo de busca 
   resultado = googlenews.result() 
-  df = pd.DataFrame(resultado)
-  lista = df['link'].tolist()
-  lista_final = ''.join(lista)
-  return render_template("noticias.html", dados = lista_final)
+  df = pd.DataFrame(resultado) #coloca o resultado em uma tabela 
+  lista = df['link'].tolist() # seleciona apenas a coluna de links de notícias da tabela e os insere em uma lista
+  lista_final = ''.join(lista) # transforma coluna de links em uma única string [texto] para importá-la e apresentá-la no site 
+  return render_template("noticias.html", dados = lista_final) # chama a variável lista_final para ser mostrada nesta seção do site 
   
 
 
