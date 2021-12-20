4c4d73f6c6db33e179ef6b4570509f0d2f17ab3e
Érika Yukari Ferreira Kamikava <89429950+erikayukari@users.noreply.github.com>
2021-12-20 18:33:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def sobre():
 @app.route('/noticias')
 def news():
   googlenews = GoogleNews()
-  googlenews.set_lang('pt-br')
+  googlenews.set_lang('pt-br') #seleciona apenas resultados  língua portuguesa
   googlenews.set_time_range(start='31/12/2019', end='31/12/2021') #seleciona a janela temporal da busca
   googlenews.get_news("'preconceito amarelo'") #seta o primeiro termo de busca 
   googlenews.get_news("'preconceito asiático'") 
@@ -27,9 +27,9 @@ def news():
   googlenews.get_news("'ódio contra asiáticos'") #seta o último termo de busca 
   resultado = googlenews.result() 
   df = pd.DataFrame(resultado) #coloca o resultado em uma tabela 
-  lista = df['link'].tolist() # seleciona apenas a coluna de links de notícias da tabela e os insere em uma lista
-  lista_final = ''.join(lista) # transforma coluna de links em uma única string [texto] para importá-la e apresentá-la no site 
-  return render_template("noticias.html", dados = lista_final) # chama a variável lista_final para ser mostrada nesta seção do site 
+  #lista = df['link'].tolist() # seleciona apenas a coluna de links de notícias da tabela e os insere em uma lista
+  #lista_final = ''.join(lista) # transforma coluna de links em uma única string [texto] para importá-la e apresentá-la no site 
+  return render_template("noticias.html", dados = resultado.to_html()) # chama a variável lista_final para ser mostrada nesta seção do site 
   
 
 
