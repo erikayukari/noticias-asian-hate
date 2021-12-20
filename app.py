2549ba5ca1b9cf82cc7d704249f5c9c9dac08e46
Érika Yukari Ferreira Kamikava <89429950+erikayukari@users.noreply.github.com>
2021-12-20 02:43:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,7 @@ from flask import Flask, render_template
 from GoogleNews import GoogleNews 
 app = Flask(__name__)
 
-def news():
-  googlenews = GoogleNews()
-  googlenews.set_time_range(start='31/12/2019', end='31/12/2019') #seleciona a janela temporal da busca
-  googlenews.get_news("'preconceito amarelo'") #seta o primeiro termo de busca 
-  googlenews.get_news("'preconceito asiático'") 
-  googlenews.get_news("'vírus chinês'")
-  googlenews.get_news("'violência contra asiáticos'")
-  googlenews.get_news("'ódio contra asiáticos'")
-  resultado = googlenews.result() 
-  return pd.DataFrame(resultado)
-  lista = df['link'].tolist()
-  lista_final = ''.join(lista)
+
 
 
 @app.route("/")
@@ -29,7 +18,18 @@ def sobre():
     return render_template("sobre.html")
 
 @app.route('/noticias')
-def raspador = news()
+def news():
+  googlenews = GoogleNews()
+  googlenews.set_time_range(start='31/12/2019', end='31/12/2019') #seleciona a janela temporal da busca
+  googlenews.get_news("'preconceito amarelo'") #seta o primeiro termo de busca 
+  googlenews.get_news("'preconceito asiático'") 
+  googlenews.get_news("'vírus chinês'")
+  googlenews.get_news("'violência contra asiáticos'")
+  googlenews.get_news("'ódio contra asiáticos'")
+  resultado = googlenews.result() 
+  df = pd.DataFrame(resultado)
+  lista = df['link'].tolist()
+  lista_final = ''.join(lista)
   return render_template("noticias.html", dados = lista_final)
   
 
