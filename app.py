4ce4905679014c9e7734228f081ea2c23530da87
Érika Yukari Ferreira Kamikava <89429950+erikayukari@users.noreply.github.com>
2021-12-19 21:23:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 from GoogleNews import GoogleNews 
 googlenews = GoogleNews()
 googlenews.set_time_range(start='31/12/2019', end='17/12/2021') #seleciona a janela temporal da busca
+googlenews.get_news("'preconceito amarelo'") #seta o primeiro termo de busca 
+resultado = googlenews.result() 
+df = pd.DataFrame(resultado)
+df.drop(columns=['desc', 'datetime', 'img', 'media']) #elimina colunas que não interessam da tabela 
+df
+
 #fim raspagem---------------------------------------
 @app.route("/")
 def hello_world():
