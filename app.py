71153794d95a9a82d7b46e4cd567642ec2aacefb
Érika Yukari Ferreira Kamikava <89429950+erikayukari@users.noreply.github.com>
2022-01-13 17:21:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def news():
   for materia in df.itertuples():
     linha = f'<a href="{materia.link}">{materia.title}</a><br>'
     dados_em_html = dados + linha
-  return render_template("noticias.html", dados=dados_em_html) # chama a variável lista_final para ser mostrada nesta seção do site 
+  return render_template("noticias.html", dados=dados_em_html()) # chama a variável lista_final para ser mostrada nesta seção do site 
   
   
 
