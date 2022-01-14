b120e513ce275bcf98fd9e41cc9c01ea7420dc7f
Daniel-EST <dd_santos@id.uff.br>
2022-01-14 09:21:57 -0300

    feat: muda o gerenciador de bibliotecas para o pipenv

--- app.py
+++ app.py
@@ -20,21 +20,22 @@ def news():
     
   googlenews = GoogleNews()
   googlenews.set_lang('pt-br') #seleciona apenas resultados  língua portuguesa
-  googlenews.set_time_range(start='31/12/2019', end='31/12/2021') #seleciona a janela temporal da busca
+  googlenews.set_time_range(start='31/12/2019', end='14/01/2022') #seleciona a janela temporal da busca
 
   googlenews.get_news("'vírus chinês'") #seta o primeiro termo de busca 
   googlenews.get_news("'preconceito amarelo'") 
   googlenews.get_news("'preconceito asiático'") 
   googlenews.get_news("'racismo asiático'")
   googlenews.get_news("'discriminação contra asiáticos'")
+  googlenews.get_news("'ataques a asiáticos'")
   googlenews.get_news("'violência contra asiáticos'")
+  googlenews.get_news("'violência contra orientais'")
   googlenews.get_news("'ódio contra asiáticos'") #seta o último termo de busca 
   resultado = googlenews.result() 
 
   df = pd.DataFrame(resultado) #coloca o resultado em uma tabela
   dados_em_html = "" #cria uma lista vazia para inserir o resultado 
   for materia in df.itertuples(): #possibilita que o código manipule o formato em que os dados serão visualizados na lista 
-    linha = f'<a href=https://"{materia.link}">{materia.title}</a><br>' #coloca o título e o link em cada linha da lista
-    dados = dados_em_html
+    linha = f'{materia.date}{materia.site}<a href="https://{materia.link}">{materia.title}</a><br>' #coloca o título e o link em cada linha da lista
     dados_em_html = dados + linha
   return render_template("noticias.html", dados = dados_em_html) # chama a variável lista_final para ser mostrada nesta seção do site 
